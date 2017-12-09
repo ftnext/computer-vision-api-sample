5c0dbe0c604b898bbd47a7b221f1c1b5127c9f3a
nikkie <takuyafjp@gmail.com>
2017-12-09 23:31:51 +0900

    サンプルコードをコピーして作成したComputerVisoionAPI呼び出しスクリプト
    * ほとんどサンプルコードのまま(https://docs.microsoft.com/en-us/azure/cognitive-services/computer-vision/quickstarts/python#analyze-an-image-with-computer-vision-api-using-python-a-nameanalyzeimage-a )
    * 執筆中のQiita記事に合わせて作成した
    * サブスクリプションキーは公開できないので他の文字に置き換えている

---
+++ document_copy.py
@@ -0,0 +1,53 @@
+########### Python 3.6 #############
+import http.client, urllib.request, urllib.parse, urllib.error, base64, json
+
+###############################################
+#### Update or verify the following values. ###
+###############################################
+
+# Replace the subscription_key string value with your valid subscription key.
+subscription_key = 'please enter your subscription key' ##### (1) #####
+
+# Replace or verify the region.
+#
+# You must use the same region in your REST API call as you used to obtain your subscription keys.
+# For example, if you obtained your subscription keys from the westus region, replace
+# "westcentralus" in the URI below with "westus".
+#
+# NOTE: Free trial subscription keys are generated in the westcentralus region, so if you are using
+# a free trial subscription key, you should not need to change this region.
+uri_base = 'westcentralus.api.cognitive.microsoft.com' ##### (2) #####
+
+headers = {
+    # Request headers.
+    'Content-Type': 'application/json',
+    'Ocp-Apim-Subscription-Key': subscription_key,
+}
+
+params = urllib.parse.urlencode({
+    # Request parameters. All of them are optional.
+    'visualFeatures': 'Description', ##### (3) #####
+    'language': 'en',
+})
+
+# Replace the three dots below with the URL of a JPEG image of a celebrity.
+body = "{'url': 'https://emotionwebsto.blob.core.windows.net/handson/emotionweb_happiness.jpg'}" ##### (4) #####
+
+try:
+    # Execute the REST API call and get the response.
+    conn = http.client.HTTPSConnection('westcentralus.api.cognitive.microsoft.com')
+    conn.request("POST", "/vision/v1.0/analyze?%s" % params, body, headers) ##### (5) #####
+    response = conn.getresponse()
+    data = response.read() ##### (6) #####
+
+    # 'data' contains the JSON data. The following formats the JSON data for display.
+    parsed = json.loads(data)
+    print ("Response:")
+    print (json.dumps(parsed, sort_keys=True, indent=2))
+    conn.close()
+
+except Exception as e:
+    print('Error:')
+    print(e)
+
+####################################
